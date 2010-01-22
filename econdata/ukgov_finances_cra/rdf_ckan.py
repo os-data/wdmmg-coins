5b773facc0cd93aaa29513380dacab6271895a98
ww <ww@irl.styx.org>
2010-01-22 15:08:09 +0100

    look for english tags first - so we don't get chinese descriptions of doap properties... should do something more intelligent here like check the accept-languages header

--- econdata/ukgov_finances_cra/rdf_ckan.py
+++ econdata/ukgov_finances_cra/rdf_ckan.py
@@ -6,9 +6,12 @@ from rdflib.graph import Graph
 cra = URIRef("http://ckan.net/package/ukgov-finances-cra")
 
 XSD = Namespace("http://www.w3.org/2001/XMLSchema#")
-CRA=Namespace("http://semantic.ckan.net/schema/ukgov-finances-cra#")
 DC=Namespace("http://purl.org/dc/terms/")
+CRA=Namespace("http://semantic.ckan.net/schema/ukgov-finances-cra#")
 SCV=Namespace("http://purl.org/NET/scovo#")
+DOAP=Namespace("http://usefulinc.com/ns/doap#")
+FOAF=Namespace("http://xmlns.com/foaf/0.1/")
+
 initNs = { "cra": CRA, "dc": DC, "rdf" : RDF, "rdfs": RDFS, "scv" : SCV }
 
 
@@ -21,6 +24,16 @@ def describe(pkgname):
 	g.add((s, RDF.type, SCV["Dataset"]))
 	g.add((s, RDFS.label, Literal(pkg["title"])))
 	g.add((s, DC["title"], Literal(pkg["title"])))
+	g.add((s, FOAF["homepage"], URIRef(pkg["url"])))
+	if pkg["license"]:
+		g.add((s, DC["rights"], Literal(pkg["license"])))
+	if pkg["notes"]:
+		g.add((s, DC["description"], Literal(pkg["notes"])))
+	if pkg["tags"]:
+		for t in pkg["tags"]:
+			g.add((s, DOAP["category"], URIRef("http://semantic.ckan.net/tags/%s" % t)))
+	if pkg["download_url"]:
+		g.add((s, DOAP["download-url"], URIRef(pkg["download_url"])))
 	return g
 
 if __name__ == '__main__':
@@ -33,6 +46,6 @@ if __name__ == '__main__':
 	ckan = ckanclient.CkanClient(api_key=argv[1])
 
 	g = describe('ukgov-finances-cra')
-	print g.identifier
+	print g.serialize()
 	cursor.delete_model(g.identifier)
 	cursor.add_model(g)
