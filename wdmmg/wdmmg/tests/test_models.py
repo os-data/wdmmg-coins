bae21f91ffd57db5f3807aee11e744b2fe9d9541
apt1002 <apt1002@cow>
2010-03-08 15:29:14 +0000

    [wdmmg/tests][m]: Beef up tests to cover key/value infrastructure.

--- wdmmg/wdmmg/tests/test_models.py
+++ wdmmg/wdmmg/tests/test_models.py
@@ -5,9 +5,9 @@ import wdmmg.model as model
 class TestAccountBasics(object):
     @classmethod
     def setup_class(self):
-        self.accname = u'acc1'
+        self.accsrc = u'acc1'
         self.accdest = u'acc2'
-        acc_src = model.Account(name=self.accname)
+        acc_src = model.Account(name=self.accsrc)
         acc_dest = model.Account(name=self.accdest)
         # transaction has the date?
         txn = model.Transaction.create_with_postings(
@@ -28,37 +28,54 @@ class TestAccountBasics(object):
         txn = model.Session.query(model.Transaction).first()
         assert txn
         assert len(txn.postings) == 2, txn
-        assert self.accname in [ posting.account.name for posting in txn.postings ]
+        assert self.accsrc in [ posting.account.name for posting in txn.postings ]
 
     def test_02(self):
-        acc_src = model.Session.query(model.Account).filter_by(name=self.accname).one()
+        acc_src = model.Session.query(model.Account).filter_by(name=self.accsrc).one()
         acc_dest = model.Session.query(model.Account).filter_by(name=self.accdest).one()
         region = model.Key(name=u'region', description=u'Area for which money was spent')
         pog = model.Key(name=u'pog', description=u'Programme Object Group')
         randomkey = model.Key(name=u'randomkey')
 
         northwest = model.EnumerationValue(name=u'Northwest', key=region)
+        northeast = model.EnumerationValue(name=u'Northeast', key=region)
         pog1 = model.EnumerationValue(name=u'surestart', key=pog)
         pog2 = model.EnumerationValue(name=u'anotherstart', key=pog)
 
-        kv = model.KeyValue(ns=u'account', object_id=acc_src.id, key=region,
+        kv = model.KeyValue(ns=u'account', account=acc_src, key=region,
                 value=northwest.name)
-
-        acc_dest.keyvalues[region] = northwest.name
+        
+        acc_src.keyvalues[region] = northeast.name # This should overwrite the KeyValue explicitly constructed above.
         acc_src.keyvalues[randomkey]= u'annakarenina'
+        acc_dest.keyvalues[region] = northwest.name
+
         model.Session.add_all([region,pog,randomkey,kv])
         model.Session.commit()
         model.Session.remove()
+        del acc_src, acc_dest, region, pog, randomkey, kv
+        
+        # Read it all back again.
 
-        pogkey = model.Session.query(model.Key).filter_by(name=u'pog').one()
-        assert pogkey.description.startswith(u'Programme')
-        assert len(pogkey.enumeration_values) == 2, pogkey
+        pog = model.Session.query(model.Key).filter_by(name=u'pog').one()
+        assert pog.description.startswith(u'Programme')
+        assert len(pog.enumeration_values) == 2, pog
 
-        regionkey = model.Session.query(model.Key).filter_by(name=u'region').one()
-        assert regionkey, regionkey
-        regionkey_kvs = model.Session.query(model.KeyValue).filter_by(key=regionkey).all()
-        assert len(regionkey_kvs) == 2, regionkey_kvs
-#        acc_kvs = model.Session.query(model.KeyValue).filter_by(account=acc)
+        region = model.Session.query(model.Key).filter_by(name=u'region').one()
+        assert region, region
+        region_kvs = model.Session.query(model.KeyValue).filter_by(key=region).all()
+        assert len(region_kvs) == 2, region_kvs
+        
+        acc_src = model.Session.query(model.Account).filter_by(name=self.accsrc).one()
+        assert acc_src
+        acc_dest = model.Session.query(model.Account).filter_by(name=self.accdest).one()
+        assert acc_dest
+        
+        acc_src_region_kv = model.Session.query(model.KeyValue).filter_by(account=acc_src).filter_by(key=region).one()
+        assert acc_src_region_kv.value == u'Northeast'
+        assert acc_src._keyvalues[region] == acc_src_region_kv
+        assert acc_src.keyvalues[region] == u'Northeast'
+        
+        assert acc_dest.keyvalues[region] == u'Northwest'
 
 
 
