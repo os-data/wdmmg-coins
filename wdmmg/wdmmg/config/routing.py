22b84fd715d68b9ae48b975211439f6647ab2dd4
apt1002 <apt1002@cow>
2010-03-08 12:22:01 +0000

    [wdmmg][s]Example of how to add a page.

--- wdmmg/wdmmg/config/routing.py
+++ wdmmg/wdmmg/config/routing.py
@@ -20,6 +20,8 @@ def make_map():
 
     # CUSTOM ROUTES HERE
 
+    map.connect('home', '/', controller='home', action='index')
+
     map.connect('/{controller}/{action}')
     map.connect('/{controller}/{action}/{id}')
 
