1cafcd600a6d0b24bddbce2f4cc95d478237332c
apt1002 <apt1002@cow>
2010-03-15 15:59:28 +0000

    [wdmmg/tests][m]: Refactor into several independent tests.

--- wdmmg/wdmmg/tests/test_craloader.py
+++ wdmmg/wdmmg/tests/test_craloader.py
@@ -19,18 +19,24 @@ class TestCRALoader(object):
         model.Session.commit()
         model.Session.remove()
 
-    def test_load(self):
+    def test_slice(self):
         print 'test_load'
         out = (model.Session.query(model.Slice)
             .filter_by(name=CRALoader.slice_name)
             ).one()
         assert out, out
+    
+    def test_govt_account(self):
         out = (model.Session.query(model.Account)
             .filter_by(name=CRALoader.govt_account_name)
             ).one()
         assert out, out
+
+    def test_number_of_accounts(self):
         out = model.Session.query(model.Account).count()
         assert out > 5, out
+    
+    def test_keys(self):
         for key_name in u'dept', u'pog', u'function', u'region':
             out = model.Session.query(model.Key).filter_by(name=key_name).one()
             assert out, key_name
