34c347f0ed17527c97c58a4fee7cac4fd6d085d9
apt1002 <apt1002@localhost>
2010-03-18 14:19:09 +0000

    [wdmmg][s]: Added version number.

--- wdmmg/wdmmg/controllers/home.py
+++ wdmmg/wdmmg/controllers/home.py
@@ -3,6 +3,7 @@ import logging
 from pylons import request, response, session, tmpl_context as c, url
 from pylons.controllers.util import abort, redirect
 
+import wdmmg
 from wdmmg.lib.base import BaseController, render
 
 log = logging.getLogger(__name__)
@@ -10,7 +11,4 @@ log = logging.getLogger(__name__)
 class HomeController(BaseController):
 
     def index(self):
-        # Return a rendered template
-        #return render('/home.mako')
-        # or, return a string
-        return 'Hello World'
+        return 'Where does my money go?: Version %s' % wdmmg.__version__
