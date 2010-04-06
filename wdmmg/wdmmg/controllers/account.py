f48f80acc4387dec307ad4514c5f4c74cb5a4796
apt1002 <apt1002@cow>
2010-04-06 13:21:37 +0100

    [wdmmg/controllers][m]: Controller for /account/id/postings .

--- wdmmg/wdmmg/controllers/account.py
+++ wdmmg/wdmmg/controllers/account.py
@@ -4,7 +4,7 @@ from pylons import request, response, session, tmpl_context as c, url
 from pylons.controllers.util import abort, redirect
 
 from wdmmg.lib.base import BaseController, render
-
+from wdmmg.lib.helpers import Page
 from wdmmg import model
 
 log = logging.getLogger(__name__)
@@ -15,5 +15,23 @@ class AccountController(BaseController):
         c.row = (model.Session.query(model.Account)
             .filter_by(id=id_)
             ).one()
+        c.num_postings = (model.Session.query(model.Posting)
+            .filter_by(account=c.row)
+            ).count()
         return render('account/view.html')
+
+    def postings(self, id_=None):
+        c.row = (model.Session.query(model.Account)
+            .filter_by(id=id_)
+            ).one()
+        query = (model.Session.query(model.Posting)
+            .filter_by(account=c.row)
+            .order_by('timestamp'))
+        c.page = Page(
+            collection=query,
+            page=int(request.params.get('page', 1)),
+            items_per_page=c.items_per_page,
+            item_count=query.count()
+        )
+        return render('account/postings.html')
 
--- wdmmg/wdmmg/tests/functional/test_account.py
+++ wdmmg/wdmmg/tests/functional/test_account.py
@@ -16,3 +16,10 @@ class TestAccountController(TestController):
         assert Fixtures.govt_account.name in response
         assert Fixtures.spender.name in response
 
+    def test_postings(self):
+        response = self.app.get(url(controller='account', action='postings',
+            id_=Fixtures.govt_account.id, items_per_page=3))
+        assert '''The following postings have been made against account''' in response
+        assert Fixtures.govt_account.name in response
+        assert '''Next''' in response
+
