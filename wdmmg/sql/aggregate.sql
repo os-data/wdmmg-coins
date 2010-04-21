31d05d3ee03a0e6f70b31c270e9953ad1d4eb03e
apt1002 <apt1002@cow>
2010-04-21 11:44:45 +0100

    [wdmmg/sql][s]: Work out an efficient population query.

--- wdmmg/sql/aggregate.sql
+++ wdmmg/sql/aggregate.sql
@@ -4,22 +4,6 @@ select * from key;
 
 select * from slice;
 
-select
-    sum(p.amount),
-    (select value from key_value where object_id = a.id
-        and key_id = '8204e14c-d4c4-4a8f-876b-b61c30d49091') as dept,
-    (select value from key_value where object_id = a.id
-        and key_id = '4722e29a-cfea-44d1-8785-96a458453e91') as region
-from account a, posting p, 'transaction' t
-where a.slice_id = 'd7b2cab1-4a4e-4313-86a6-828a63535616' -- CRA
-and a.id = p.account_id
-and a.id not in (select object_id from key_value where value = 'yes'
-    and key_id = '0b05ee0b-6a20-4ed3-b36f-329888908dcc') -- spender
-and t.id = p.transaction_id
-and t.timestamp >= '1000-01-01'
-and t.timestamp < '3000-01-01'
-group by dept, region;
-
 -- New version.
 
 SELECT
@@ -41,12 +25,14 @@ AND t.id = p.transaction_id
 --AND t.timestamp < :end_date
 GROUP BY t.timestamp, axis_0, axis_1
 ORDER BY t.timestamp, axis_0, axis_1
+;
 
-ak_1 1b3a4ebf-a599-4f9f-b240-a3945969ea58
-ak_0 6f681fe5-6cba-4063-bdde-d91e2d5f8ee5
-v_0 yes%
-v_1 ENGLAND%
-slice_id aeab1d75-6655-4ca6-aa45-5e0c1302c341
-k_1 d13836fd-876e-4432-b308-a06713d03d02
-k_0 a2281133-553b-494a-acaa-654518a5d090
+-- Population
+
+SELECT ev.name, kv.value
+FROM enumeration_value ev, key_value kv
+WHERE ev.key_id = '8dc888de-27a7-41f3-ba80-c4c65a46f286'
+AND kv.key_id = '18608816-2079-48cb-9ccc-97577710164f'
+AND kv.object_id = ev.id
+;
 
