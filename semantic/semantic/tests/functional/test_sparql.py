736fef0c1b113ee2751445c4be5cabb4e531a356
rgrp <http://rufuspollock.org@localhost>
2010-04-28 12:07:25 +0100

    [semantic][m]: remove semantic subproject as no longer needed (most of it has become ckan semantic project/repo).

--- semantic/semantic/tests/functional/test_sparql.py
+++
@@ -1,7 +0,0 @@
-from semantic.tests import *
-
-class TestSparqlController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='sparql', action='index'))
-        # Test response...
