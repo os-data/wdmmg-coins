383a1be92c1e7f6690ac0eb40b30df53142204dc
rgrp <http://rufuspollock.org@localhost>
2010-05-01 13:02:46 +0100

    [wdmmg/wui,transaction][s]: improve transaction index page by having some intro text plus browse list of all transactions.

--- wdmmg/wdmmg/controllers/transaction.py
+++ wdmmg/wdmmg/controllers/transaction.py
@@ -4,13 +4,20 @@ from pylons import request, response, session, tmpl_context as c, url
 from pylons.controllers.util import abort, redirect
 
 from wdmmg.lib.base import BaseController, render
-
+from wdmmg.lib.helpers import Page
 from wdmmg import model
 
 log = logging.getLogger(__name__)
 
 class TransactionController(BaseController):
     def index(self):
+        query = model.Session.query(model.Transaction)
+        c.page = Page(
+            collection=query,
+            page=int(request.params.get('page', 1)),
+            items_per_page=c.items_per_page,
+            item_count=query.count(),
+        )
         return render('transaction/index.html')
 
     def view(self, id_=None):
--- wdmmg/wdmmg/tests/functional/test_transaction.py
+++ wdmmg/wdmmg/tests/functional/test_transaction.py
@@ -3,7 +3,6 @@ from wdmmg.tests import *
 import wdmmg.model as model
 
 class TestTransactionController(TestController):
-
     @classmethod
     def setup_class(self):
         Fixtures.setup()
@@ -11,6 +10,12 @@ class TestTransactionController(TestController):
     @classmethod
     def teardown_class(self):
         Fixtures.teardown()
+
+    def test_index(self):
+        response = self.app.get(url(controller='transaction', action='index'))
+        assert '36 transactions' in response, response
+        # name of an account
+        assert 'Government (Dummy)' in response, response
 
     def test_view(self):
         t = model.Session.query(model.Transaction).first()
