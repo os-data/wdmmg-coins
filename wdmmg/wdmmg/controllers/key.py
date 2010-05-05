e6a56c961678196d74824f6dee3f740381b7925d
apt1002 <apt1002@cow>
2010-05-05 12:22:49 +0100

    [wdmmg/controllers][m]: Group keys that are hierarchically related.

--- wdmmg/wdmmg/controllers/key.py
+++ wdmmg/wdmmg/controllers/key.py
@@ -12,7 +12,26 @@ log = logging.getLogger(__name__)
 class KeyController(BaseController):
 
     def index(self):
-        c.results = sorted(model.Session.query(model.Key), key=lambda k: k.name)
+        all_keys = model.Session.query(model.Key).all()
+        # Make ourselves an index of keys by name.
+        c.by_name = {} # name -> Key
+        for key in all_keys:
+            c.by_name[key.name] = key
+        parent_key = self.get_by_name_or_id(model.Key, name_or_id=u'parent')
+        # Identify keys with/without parents.
+        c.parents = {} # name -> name
+        c.by_parent = {} # name -> list of names
+        for key in all_keys:
+            if parent_key in key.keyvalues:
+                c.parents[key.name] = key.keyvalues[parent_key]
+            else:
+                c.by_parent[key.name] = []
+        # For each key that has a parent, attach it to its ultimate ancestor.
+        for name in c.parents:
+            parent = c.parents[name]
+            while parent in c.parents:
+                parent = c.parents[parent]
+            c.by_parent[parent].append(name)
         return render('key/index.html')
 
     def view(self, name_or_id=None):
