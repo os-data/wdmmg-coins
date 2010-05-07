5976e7aa4849a324174b4d7755551dd8ca6af6a4
apt1002 <apt1002@cow>
2010-05-07 16:46:06 +0100

    [wdmmg][m]: Income tax estimation: real data and interpolation. Fall-back spending estimation.

--- wdmmg/wdmmg/lib/calculator.py
+++ wdmmg/wdmmg/lib/calculator.py
@@ -1,8 +1,39 @@
-income_table = {
-    0: 0,
-    10000: 2000,
-    20000: 5000,
-}
+import csv
+from StringIO import StringIO
+
+# National statistics for income bands.
+# In each band, column 1 is the average income, and column 2 is the average tax.
+# This comes from table 2-5 of http://www.hmrc.gov.uk/stats/income_tax/menu.htm .
+# We use the 2007-08 data, which is the latest year surveyed.
+# Columns 0, 1, 2 and 4 are from the source data.
+# Column 3 is the ratio of column 1 to column 2.
+# We map from column 3 to column 4.
+# We connect the data points by linear interpolation.
+# We add a data point for the tax-free personal allowance.
+# For very high incomes, we use a constant tax/income ratio.
+reader = csv.reader(StringIO('''\
+"Low income","total income","count","average income","tax"
+5225,14700000000,2290000,6419.21,111
+7500,29800000000,3400000,8764.71,378
+10000,82200000000,6600000,12454.55,1110
+15000,94600000000,5430000,17421.73,2200
+20000,168000000000,6850000,24525.55,3720
+30000,201000000000,5340000,37640.45,6460
+50000,125000000000,1900000,65789.47,16200
+100000,39600000000,328000,120731.71,36000
+150000,21900000000,128000,171093.75,54500
+200000,43100000000,149000,289261.74,96300
+500000,19200000000,28000,685714.29,235000
+1000000,31700000000,14000,2264285.71,823000
+'''))
+headings = reader.next()
+income_table = list(reader)
+income_table = (
+    [(income_table[0][0], 0)] + # Tax-free personal allowance.
+    [(row[3], row[4]) for row in income_table] # Columns 3 and 4.
+)
+income_table = [(float(income), float(tax)) for income, tax in income_table]
+income_top_rate = income_table[-1][1] / income_table[-1][0] # For very high incomes.
 
 vat_rate = 0.175
 population = 61.0e6
@@ -15,29 +46,39 @@ tobacco_population = population * 0.21
 
 def tax_share(
     income,
-    disposable_income,
-    is_smoker,
-    is_driver
+    spending = None,
+    is_smoker = False,
+    is_driver = False
 ):
     '''
     Estimates a person's tax contribution based on the following information:
     
     income - Total personal income. This is used to estimate income tax paid.
-    disposable_income - Household expenditure. This is used to estimate VAT paid.
+    spending - Household expenditure. This is used to estimate VAT paid.
     is_smoker - `True` if the person smokes. This adds a share of tobacco duty.
     is_driver - `True` if the person drives a car. This adds a share of fuel duty.
     '''
     tax = 0.0
     # Income tax: Calculate which income range the income falls in.
-    bucket = 0
-    for k, v in income_table.items():
-        if k <= income and k>bucket:
-            bucket = k
-    tax += income_table[bucket]
+    lower = (0.0, 0.0)
+    for upper in income_table:
+        if upper[0] >= income:
+            # Found the right band. Use linear interpolation.
+            income_tax = lower[1] + (income-lower[0]) * (upper[1]-lower[1]) / (upper[0]-lower[0])
+            break
+        else:
+            lower = upper
+    else:
+        # Above all the bands. Use constant tax rate.
+        income_tax = income * income_top_rate
+    tax += income_tax
     # NI contributions are divided equally across the population.
     tax += ni_total / population
-    # VAT: Multiply disposable income by `vat_rate`.
-    tax += disposable_income * vat_rate
+    # VAT: Multiply spending by `vat_rate`.
+    if spending is None:
+        # Estimate based on income.
+        spending = income * 0.8
+    tax += spending * vat_rate
     # Corporation tax is divided equally across the population.
     tax += ct_total / population
     # Fuel duty: if a driver, fuel duty is divided equally across drivers.
--- wdmmg/wdmmg/tests/test_calculator.py
+++ wdmmg/wdmmg/tests/test_calculator.py
@@ -1,8 +1,25 @@
 from wdmmg.lib import calculator
 
-def test_tax_share():
-    ans = calculator.tax_share(30000, 12000, True, True)
-    assert 3000<ans<15000, ans
+constant_tax = (calculator.ni_total + calculator.ct_total) / calculator.population
+
+def test_income_tax():
+    def test(i, t):
+        ans = calculator.tax_share(i, spending=0) - constant_tax
+        assert abs(ans - t) < 1, ans
+    # Low incomes
+    test(0, 0)
+    test(5225, 0)
+    # High incomes
+    test(2264285.71, 823000)
+    test(10e6, 3634700)
+    # Mid incomes, requiring interpolation.
+    test(25837.04, 3994)
+
+def test_vat():
+    ans = calculator.tax_share(0, spending=100) - constant_tax
+    assert abs(ans - 17.5) < 1, ans
+    ans = calculator.tax_share(100) - constant_tax
+    assert abs(ans - 14) < 1, ans
 
 def test_spending_share():
     test_tax = 5000
