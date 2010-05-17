8caf452d6526e0d8b91da8eef8f4ae96acc05b0c
apt1002 <apt1002@cow>
2010-05-17 15:38:27 +0100

    [wdmmg/getdata][m]: Overwrite official COFOG names with de-jargonised alternatives (back up official names).

--- wdmmg/wdmmg/getdata/cofog.py
+++ wdmmg/wdmmg/getdata/cofog.py
@@ -4,6 +4,7 @@ from StringIO import StringIO
 import os, csv
 
 import datapkg
+from swiss.tabular import gdocs
 from pylons import config
 
 import wdmmg.model as model
@@ -41,8 +42,49 @@ def load():
     # Get the COFOG data package.
     pkgspec = 'file://%s' % os.path.join(config['getdata_cache'], 'cofog')
     pkg = datapkg.load_package(pkgspec)
-    fileobj = pkg.stream('cofog.csv') # FIXME: Something.csv
+    fileobj = pkg.stream('cofog.csv')
     load_file(fileobj)
+
+def dejargonise():
+    '''
+    Downloads alternative names for the COFOG codes, chosen specially for WDMMG,
+    and replaces the names in the database. The original names are backed up.
+    '''
+    keys = [model.Session.query(model.Key).filter_by(name=name).one()
+        for name in u'cofog1', u'cofog2', u'cofog3']
+    # Create the 'official_name' Key if necessary.
+    key_official_name = (model.Session.query(model.Key)
+        .filter_by(name=u'official_name')
+        ).first()
+    if not key_official_name:
+        key_official_name = model.Key(name=u'official_name', notes=u'''\
+The official names of things, as defined by the relevant standards body. We \
+do not always use the official names for things, because they are sometimes \
+difficult to understand. When we substitute an alternative name, we record \
+the official name using this key.''')
+        model.Session.add(key_official_name)
+    # Loop through the rows of the Google spreadsheet where the data is maintained.
+    for row in gdocs.GDocsReaderTextDb(
+        'tQSJ9dxTh8AKl-ON4Qqja8Q', # Google spreadsheet key.
+        config['gdocs_username'],
+        config['gdocs_password']
+    ).read().to_list()[2:]:
+#        print row
+        code, official_name, alternative_name, notes = row
+        ev = (model.Session.query(model.EnumerationValue)
+            .filter(model.EnumerationValue.key_id.in_([k.id for k in keys]))
+            .filter_by(code=unicode(code))
+            ).first()
+        assert ev, 'Spreadsheet contains an unknown COFOG code: %r' % row
+        if alternative_name:
+            print "Setting name of %r to %r" % (code, alternative_name)
+            if key_official_name not in ev.keyvalues:
+                # Make a backup of the official name.
+                # assert ev.name == official_name
+                ev.keyvalues[key_official_name] = ev.name
+            ev.name = unicode(alternative_name)
+    model.Session.commit()
+    model.Session.remove()
 
 def load_file(fileobj):
     '''
